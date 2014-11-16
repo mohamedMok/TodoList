dd84a2f9ca5245bb6f2d3206b533d47d35b66095
mohamedMok <Mohamed@192.168.1.29>
2014-11-16 23:18:23 +0100

    Get and Delete by Id method

--- todolist/src/fr/resources/TasksResource.java
+++ todolist/src/fr/resources/TasksResource.java
@@ -2,8 +2,11 @@ package fr.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,6 +14,11 @@ import javax.ws.rs.core.Response;
 import fr.dao.TaskDAO;
 import fr.model.Task;
 
+/**
+ * 
+ * @author Mohamed M
+ *
+ */
 @Path("/tasks")
 public class TasksResource {
 	private TaskDAO dao = new TaskDAO();
@@ -25,5 +33,30 @@ public class TasksResource {
 			return Response.ok().entity(tasks).build();
 		}
 	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTaskById(@PathParam(value="id") String id){
+		Task t = dao.getTaskById(id);
+		if(t == null)
+			return Response.status(400).entity("Les paramètres entrés sont incorrects (id inexistant ?)").build();
+		else
+			return Response.ok().entity(t).build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteTask(@PathParam(value="id") String id){
+		boolean deleted = dao.deleteTask(id);
+        if(!deleted){
+                return Response.status(400).entity("Les paramètres entrés sont incorrects (id inexistant ?)").build();
+        }else{
+                return Response.ok().build();
+        }
+		
+	}
+	
 	
 }
